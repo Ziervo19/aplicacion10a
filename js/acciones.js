f6db54648bada66010b8982d53cfe9979f2ae475
Ziervo19 <albertoziervo@hotmail.com>
2016-05-24 12:30:44 -0500

    1subida

---
+++ js/acciones.js
@@ -0,0 +1,25 @@
+//JavaScriot
+ $(document).ready(function (e){ 
+ $('#principal').height($('#page1').height());
+ document.addEventListener("deviceready",function(){
+
+ audio=window .plugins .LowLatencyAudio;
+ audio.preloadFX('do','audio/DO.mp3',function(){},function(e){alert('Error'+e);});
+ audio.preloadFX('re','audio/RE.mp3',function(){},function(e){alert('Error'+e);});
+ audio.preIoadFX('mi','audio/MI.mp3',function(){},function(e){alert('Error'+e);});
+ audio.preloadFX('fa','audio/FA,mp3',function(){},function(e){alert('Error'+e);});
+ audio.preloadFX('sol','audio/SQL.mp3',function(){},function(e){alert('Error'+e);});
+ audio.preloadFX('la','audio/LA.mp3',function(){},function(e){alert('Error'+e);});
+ audio.preIoadFX('si','audio/SI.mp3',function(){},function(e){alert('Error'+e);});
+
+ $('.nota').bind('touchstart',function(){
+ $(this).addClass('tocada');
+ audio.play($(this).attr('id'));
+
+ }).bind('touchend',function(){
+ $(this).removeClass('tocada');
+ });
+
+ },false);
+
+ });
